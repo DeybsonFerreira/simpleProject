ca0ce457a32a1a2413968b43e42e0153795db259
Deybson Ferreira <deybson.ferreira@iatec.com>
2021-02-25 12:47:18 -0300

    add project

---
+++ Bootstrap_/Bootstrap_/Controllers/HomeController.cs
@@ -0,0 +1,52 @@
+﻿using Bootstrap_.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Bootstrap_.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Dica1()
+        {
+            return View();
+        }
+        public IActionResult Dica2()
+        {
+            return View();
+        }
+        public IActionResult Dica3()
+        {
+            return View();
+        }
+        public IActionResult Dica4()
+        {
+            return View();
+        }
+        public IActionResult Dica5()
+        {
+            return View();
+        }
+        public IActionResult Dica6()
+        {
+            return View();
+        }
+        public IActionResult Dica7()
+        {
+            return View();
+        }
+        public IActionResult Dica8()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Bootstrap_/Bootstrap_/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Bootstrap_.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
